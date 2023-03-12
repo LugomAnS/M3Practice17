f40a06bf28d7d1b8e7df24333d6b6c5a626f4837
Anton Lugom <mogul086@mail.ru>
2023-03-12 16:45:16 +0300

    AccessDB Scaffold

---
+++ EFAccess/Contexts/ModelContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using EFAccess.Models;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace EFAccess.Contexts
+{
+    public partial class ModelContext : DbContext
+    {
+        public ModelContext()
+        {
+        }
+
+        public ModelContext(DbContextOptions<ModelContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Purchases> Purchases { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseJet("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=AccessDB.accdb");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Purchases>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("counter");
+
+                entity.Property(e => e.EMail)
+                    .IsRequired()
+                    .HasColumnName("eMail")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.ItemCode).HasColumnName("itemCode");
+
+                entity.Property(e => e.ItemName)
+                    .IsRequired()
+                    .HasColumnName("itemName")
+                    .HasMaxLength(255);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
